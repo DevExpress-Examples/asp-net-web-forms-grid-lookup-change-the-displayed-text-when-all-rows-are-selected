ca3091ea1591becf9fbf81f2d32e9e19ab91c228
DevExpressExampleBot <fakeEmail>
2018-04-24 00:31:45 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Web
@@ -8,15 +7,16 @@ Imports System.Web.UI.WebControls
 Imports DevExpress.Web.ASPxGridLookup
 
 Partial Public Class _Default
-	Inherits System.Web.UI.Page
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+    Inherits System.Web.UI.Page
 
-	End Sub
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
 
-	Protected Sub gridLookup_ValueChanged(ByVal sender As Object, ByVal e As EventArgs)
-		Dim gl As ASPxGridLookup = TryCast(sender, ASPxGridLookup)
-		If gl.GridView.Selection.Count = gl.GridView.VisibleRowCount Then
-			gl.GridView.JSProperties("cp_selected") = True
-		End If
-	End Sub
+    End Sub
+
+    Protected Sub gridLookup_ValueChanged(ByVal sender As Object, ByVal e As EventArgs)
+        Dim gl As ASPxGridLookup = TryCast(sender, ASPxGridLookup)
+        If gl.GridView.Selection.Count = gl.GridView.VisibleRowCount Then
+            gl.GridView.JSProperties("cp_selected") = True
+        End If
+    End Sub
 End Class
