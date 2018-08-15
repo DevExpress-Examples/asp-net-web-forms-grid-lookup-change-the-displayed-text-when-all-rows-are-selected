b01311bd4a9ac45c63d72445511fdbae8e0c4556
DevExpressExampleBot <fakeEmail>
2018-08-15 18:53:31 +0300

    Source auto update [skip ci]

--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -5,6 +5,7 @@ Imports System.Web
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports DevExpress.Web.ASPxGridLookup
+Imports DevExpress.Web.ASPxClasses
 
 Partial Public Class _Default
     Inherits System.Web.UI.Page
@@ -13,10 +14,22 @@ Partial Public Class _Default
 
     End Sub
 
-    Protected Sub gridLookup_ValueChanged(ByVal sender As Object, ByVal e As EventArgs)
-        Dim gl As ASPxGridLookup = TryCast(sender, ASPxGridLookup)
+     Protected Sub gridLookup_ValueChanged(ByVal sender As Object, ByVal e As EventArgs)
+        SpecifyJSProperties(TryCast(sender, ASPxGridLookup))
+     End Sub
+
+    Protected Sub gridLookup_CustomJSProperties(ByVal sender As Object, ByVal e As CustomJSPropertiesEventArgs)
+        SpecifyJSProperties(TryCast(sender, ASPxGridLookup))
+    End Sub
+    Protected Sub SpecifyJSProperties(ByVal gl As ASPxGridLookup)
         If gl.GridView.Selection.Count = gl.GridView.VisibleRowCount Then
             gl.GridView.JSProperties("cp_selected") = True
+        End If
+    End Sub
+
+    Protected Sub gridLookup_DataBound(ByVal sender As Object, ByVal e As EventArgs)
+        If Not IsPostBack Then
+            TryCast(sender, ASPxGridLookup).GridView.Selection.SelectAll()
         End If
     End Sub
 End Class
